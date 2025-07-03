ddc4310579d1dfd8ad7692b733b29a2e43d51ce0
ArmandoRodriguezV <231184@ids.upchiapas.edu.mx>
2025-07-03 11:09:50 -0600

    V1.0.3 Individuo añadido

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ from src.test.main_model_SQL import mostrar_tabla_completa
 
 i = Individual(['R1', 'R2', 'R9'])
 
-print(i.metric_1)
+print(i.habs_no_aprob)
+print(i.habs_aprob)
+print(i.data)
 print(i)
 
-mostrar_tabla_completa()
+# mostrar_tabla_completa()
--- src/models/individual/individual.py
+++ src/models/individual/individual.py
@@ -8,17 +8,30 @@ class Individual:
         self.habs_aprob = []
         self.habilidades_no_aprobadas()
         
+        self.habs_no_aprob_por_r = []
         
-        self.metric_1 = len(self.habs_no_aprob) / len(self.habs_aprob + self.habs_no_aprob)
+        self.fitness = round(len(self.habs_no_aprob) / len(self.habs_aprob + self.habs_no_aprob), 2)
+        
+        self.data = {
+            "HNA": len(self.habs_no_aprob),
+            "HA": len(self.habs_aprob),
+            "Fitness": self.fitness
+        }
         
     def habilidades_no_aprobadas(self):        
         for x in self.gens:
             for y in reactivos[x]:
                 cal = hab[y]
                 if cal < 0.7:
-                    self.habs_no_aprob.append(cal)
+                    self.habs_no_aprob.append(y)
                 else:
-                    self.habs_aprob.append(cal)
+                    self.habs_aprob.append(y)
+    
+    def get_reactives(self):
+        reactives = []
+        for g in self.gens:
+            reactives.append(reactivos[g])
+        return reactivos
     
     def __str__(self):
         return mostrar_tabla_de(self.gens)
