3aa10fd19a1b535b59fdf9f9c1664ee20211b81b
ArmandoRodriguezV <231184@ids.upchiapas.edu.mx>
2025-07-03 11:20:26 -0600

    V1.0.4 Entorno Agregado

---
+++ src/models/environment/environment.py
@@ -0,0 +1,6 @@
+from typing import List
+from ..individual.individual import Individual
+
+class Envioronment:
+    def __init__(self, poblacion: List[Individual]):
+        self.poblacion = poblacion
