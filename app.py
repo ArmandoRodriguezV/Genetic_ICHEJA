3e818a7ce2e518845d6910df4672e41125054b9e
ArmandoRodriguezV <231184@ids.upchiapas.edu.mx>
2025-07-03 15:13:55 -0600

    V1.0.9 Poblacion

--- app.py
+++ app.py
@@ -4,14 +4,11 @@ from src.models.environment.environment import Envioronment
 from src.test.main_model_SQL import mostrar_tabla_completa, reactivos
 
 poblacion = []
-
 claves_reactivos = list(reactivos.keys())
 
 for _ in range(20):
     seleccionados = sample(claves_reactivos, 10)
     individuo = Individual(seleccionados)
     poblacion.append(individuo)
-
-# Mostrar los genes de cada individuo
-for i, ind in enumerate(poblacion):
-    print(f"Individuo {i+1}: {ind.gens}")
+    
+entorno = Envioronment(poblacion=poblacion)
