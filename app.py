fb3714a32863194ad45ec6f5dbef78f2b506cd65
ArmandoRodriguezV <231184@ids.upchiapas.edu.mx>
2025-07-04 10:55:04 -0600

    V1.1.2

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
 from random import sample
 from src.models.individual.individual import Individual
 from src.models.environment.environment import Envioronment
-from src.test.main_model_SQL import mostrar_tabla_MRH
+from src.test.main_model_SQL import *
 
-mostrar_tabla_MRH()
+poblacion = [
+    Individual(sample([f"R{i}" for i in range(1, 21)], 10)),
+    Individual(sample([f"R{i}" for i in range(1, 21)], 10)),
+    Individual(sample([f"R{i}" for i in range(1, 21)], 10)),
+    Individual(sample([f"R{i}" for i in range(1, 21)], 10)),
+    Individual(sample([f"R{i}" for i in range(1, 21)], 10)),
+    Individual(sample([f"R{i}" for i in range(1, 21)], 10)),
+    Individual(sample([f"R{i}" for i in range(1, 21)], 10)),
+    Individual(sample([f"R{i}" for i in range(1, 21)], 10)),
+    Individual(sample([f"R{i}" for i in range(1, 21)], 10)),
+    Individual(sample([f"R{i}" for i in range(1, 21)], 10)),
+]
+
+print(f"población inicial {len(poblacion)}")
+entorno = Envioronment(poblacion=poblacion, generations=5)
+entorno.start()
+
+entorno.print_pob(True)
+print(f"población final {len(entorno.poblacion)}")
--- src/models/environment/environment.py
+++ src/models/environment/environment.py
@@ -1,4 +1,5 @@
 from random import sample, randint
+from math import ceil
 from typing import List
 from ..individual.individual import Individual
 
@@ -6,10 +7,12 @@ class Envioronment:
     def __init__(self, poblacion: List[Individual], generations: int = 1):
         self.poblacion = poblacion
         self.generations = generations
+        self.size = len(self.poblacion)
         
     def start(self):
         for _ in range(self.generations):
             self.crosses()
+            self.poda()
     
     def select_pair(self):
         p1, p2 = sample(range(len(self.poblacion)), 2)
@@ -26,9 +29,27 @@ class Envioronment:
             self.poblacion.append(h1)
             self.poblacion.append(h2)
         
-    def print_pob(self, show_table: bool):
+    def poda(self):
+        self.poblacion = sorted(self.poblacion, key=lambda i: i .fitness)
+        mejor_padre = self.poblacion[0]
+        mejor_madre = self.poblacion[1]
+        
+        mitad_sin_padres = (len(self.poblacion) - 2)//2
+        
+        sub_1 = self.poblacion[2 + mitad_sin_padres:]
+        sub_2 = self.poblacion[2 : mitad_sin_padres + 2]
+        
+        mitad_sub_grup = self.size / 2
+        primera_mitad = sample(sub_1, ceil(mitad_sub_grup) - 2)
+        segunda_mitad = sample(sub_2, int(mitad_sub_grup))
+        
+        self.poblacion.clear()
+        self.poblacion.append(mejor_padre)
+        self.poblacion.append(mejor_madre)
+        self.poblacion += primera_mitad
+        self.poblacion += segunda_mitad
+
+    def print_pob(self, show_table: bool = False):
         for i in self.poblacion:
             i.show_table = show_table
             print(i)
-
-
